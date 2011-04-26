ff953c58da7300bf475a02d08e00b4dc7cac86e7
Tero Piirainen <tipiirai@gmail.com>
2011-04-26 06:15:29 +0300

    removed jQuery dependency on tests

--- tests/css3.js
+++ tests/css3.js
@@ -1,17 +1,20 @@
+
 /**
  * Unittests: CSS3
  */
 
+var klasses = " " + document.documentElement.className;
+
+function hasClass(klass) {
+    return klasses.indexOf(" " + klass) !== -1;
+}
+
 module('CSS3');
 
 test("Feature detection", function() {
-	ok(
-		($('html').hasClass('boxshadow') || $('html').hasClass('no-boxshadow')),
-		'html element should contain a boxshadow feature detection class'
-	);
-
-	ok(
-		($('html').hasClass('boxshadow') !== $('html').hasClass('no-boxshadow')),
-		'A feature can\'t both be avaiable and not be available'
-	);
+    
+    ok(hasClass('boxshadow') || hasClass('no-boxshadow'), 'html element contains boxshadow feature detection');
+    
+    ok(hasClass('boxshadow') !== hasClass('no-boxshadow'), 'feature and no-feature cannot co-exist');
+    
 });
