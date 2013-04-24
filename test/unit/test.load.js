71f797bf895337f3aeba01c0f330ce41245badf5
Igor Lima <igor.lima@evolut.io>
2013-04-23 21:39:33 -0300

    Put test for loading css file in another file

--- test/unit/test.load.js
+++ test/unit/test.load.js
@@ -1,18 +1,5 @@
 module('head.load.js');
 
-function getStyle(ele, styleProp) {
-    var y = "";
-
-    if (ele.currentStyle) {
-        y = ele.currentStyle[styleProp];
-    }
-    else if (window.getComputedStyle) {
-        y = document.defaultView.getComputedStyle(ele, null).getPropertyValue(styleProp);
-    }
-    
-    return y;
-}
-
 asyncTest("jquery, mootools (trigger via callback)", function() {
     expect(2);
     
@@ -71,19 +58,4 @@ asyncTest('jshint, jquery, knockout (trigger via label)', function () {
         
         start();
     });       
-});
-
-
-// INFO: will make had fail (and nothing else continues!) if file not exists
-asyncTest("css (load)", function () {
-    expect(1);
-
-    head.ready("test.css", function () {
-        var result = getStyle(document.getElementById("browserscope"), "display");
-        ok(result === "block", "Filename: ready('test.css')");
-
-        start();
-    });
-
-    head.js("assets/test.css");
 });
