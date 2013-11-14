b5c23ed2ae5e8da4a0cfc0e0f579ad0b3b5e014f
robert-hoffmann <mail.someone@gmail.com>
2013-11-14 09:59:54 +0100

    refactoring

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,22 @@
 module.exports = function (grunt) {
+
+    // Project configuration
     grunt.initConfig({
+        // load package information
+        pkg: grunt.file.readJSON('package.json'),
+
+        // task: unit tests
         qunit: {
             files: ['test/unit/1.0.0/index-travis.html']
         }
     });
 
+
     grunt.loadNpmTasks('grunt-contrib-qunit');
 
-    // A convenient task alias.
+
+
+
+    // register: unit tests
     grunt.registerTask('test', 'qunit');
 };
